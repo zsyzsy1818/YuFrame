5c7d868a06701b8fa29ff58bfd791361b8a23bed
Yu <zsyzsy1818@hotmail.com>
2016-04-18 00:40:30 +0800

    加入  神兽一只,呵呵

--- app/src/main/java/com/yu/yuframe/ui/MainActivity.java
+++ app/src/main/java/com/yu/yuframe/ui/MainActivity.java
@@ -3,6 +3,28 @@ package com.yu.yuframe.ui;
 import android.os.Bundle;
 
 import com.yu.yuframe.R;
+/**
+ *
+ *----------Dragon be here!----------/
+ *         　　 ┏┓　　　┏┓
+ * 　       　┏┛┻━━━┛┻┓
+ * 　      　┃　　　　　　　┃
+ *      　　┃　　　━　　　┃
+ * 　    　┃　┳┛　┗┳　┃
+ * 　   　┃　　　　　　　┃
+ * 　　  ┃　　　┻　　　┃
+ * 　 　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　 ┃　　　┃神兽保佑
+ * 　　　┃　　　┃代码无BUG！
+ * 　　 ┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　 ┃　　　　　　　┏┛
+ * 　┗┓┓┏━┳┓┏┛
+ * 　 ┃┫┫　┃┫┫
+ * 　┗┻┛　┗┻┛
+ * ━━━━━━神兽出没━━━━━━by:Yu
+ */
 
 public class MainActivity extends BaseActivity {
 
